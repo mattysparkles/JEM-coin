18728527a54535dc421fbd716381641d5ef019d4
Matthew Sparkles <128464775+mattysparkles@users.noreply.github.com>
2025-08-20 18:16:49 -0400

    feat: implement bech32 address encoding

--- clients/wallet-core/test/index.test.ts
+++ clients/wallet-core/test/index.test.ts
@@ -1,8 +1,17 @@
+import { bech32 } from 'bech32';
 import { addressFromPublicKey } from '../src';
 
-const pk = new Uint8Array([1,2,3]);
+const pk = new Uint8Array([1, 2, 3]);
 const addr = addressFromPublicKey(pk);
-if (!addr.startsWith('jem')) {
+const decoded = bech32.decode(addr);
+
+if (decoded.prefix !== 'jem') {
   throw new Error('address must start with jem');
 }
+
+const bytes = new Uint8Array(bech32.fromWords(decoded.words));
+if (bytes.toString() !== pk.toString()) {
+  throw new Error('decoded bytes mismatch');
+}
+
 console.log('address', addr);
